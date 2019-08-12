06845b92044802bb280931037be900eb5778c55f
adelashraf <adeltttttt@ymail.com>
2019-08-12 22:42:34 +0200

    0

--- app/src/main/java/com/onepound/dialogs/Dialogs.kt
+++ app/src/main/java/com/onepound/dialogs/Dialogs.kt
@@ -13,6 +13,9 @@ import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.widget.ButtonBarLayout
 import androidx.core.view.get
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
 import kotlinx.android.synthetic.main.normal.*
 
 
@@ -39,12 +42,19 @@ class Dialogs(context: Context) : Dialog(context) {
     fun D2(context: Context, vi: Int) {
         val builde = Dialog(context)
         val But = Button(context)
-        var Weda = androidx.constraintlayout.widget.ConstraintLayout(context)
-        Weda.addView(But)
-        Weda.layout(100, 100, 100, 100)
+        var Tex = TextView(context)
+        Tex.height = 100
+        Tex.width = 190
+        Tex.marginLeft
+        Tex.marginRight
+        Tex.text = "A"
         But.text = "OkK"
         But.height = 50
-        But.width = 50
+        But.width = 150
+        var Weda = androidx.constraintlayout.widget.ConstraintLayout(context)
+        Weda.layout(100, 100, 100, 100)
+        Weda.addView(Tex)
+        Weda.addView(But)
         builde.addContentView(Weda, ViewGroup.LayoutParams(500, 500))
         //builde.setContentView(vi)
         builde.setTitle("Adel")
@@ -54,6 +64,9 @@ class Dialogs(context: Context) : Dialog(context) {
         builde.Normal_Yes.setOnClickListener {
             builde.Normal_7amada.text = "He Say Yes!!"
         }*/
+        But.setOnClickListener {
+            Tex.text = "You Bressing Okk"
+        }
         builde.create()
         builde.show()
 
